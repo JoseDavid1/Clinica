7516a8aa5dd034f9edc451b05c05f8289f55f302
Oswaldo-PC\Oswaldo <linares.guerraj@gmail.com>
2018-09-20 16:02:13 -0600

    query para insertar la consulta

--- paciente/insertConsultaExtendida.php
+++ paciente/insertConsultaExtendida.php
@@ -2,12 +2,31 @@
 include('../conexion.php');
 session_start();
 
+#Datos de consulta normal
 $paciente = $_SESSION['pacienteActivo'];
 $fecha = $_POST['present'];
 $motivo = $_POST['motivoConsulta'];
 $historia = $_POST['historiaConsulta'];
 $diagnostico = $_POST['diagnostico'];
 $tratamiento = $_POST['tratamiento'];
+
+#Datos consulta Extendida
+$frecuencia = $_POST['frecuencia'];
+$presion = $_POST['presion'];
+$saturacion = $_POST['saturacion'];
+$temperatura = $_POST['temperatura'];
+$respiratoria = $_POST['respiratoria'];
+$piel = $_POST['piel'];
+$cabeza = $_POST['cabeza'];
+$cuello = $_POST['cuello'];
+$torax = $_POST['torax'];
+$pulmones = $_POST['pulmones'];
+$corazon = $_POST['corazon'];
+$abdomen = $_POST['abdomen'];
+$ginecologico = $_POST['ginecologico'];
+$extremidades = $_POST['extremidades'];
+$neurologico = $_POST['neurologico'];
+
 
 function generarCodigo($longitud, $tipo=0)
 		{
@@ -35,13 +54,19 @@ $insertarConsulta = mysqli_query($conexion,$nuevaConsulta);
 
 	}else{
 		#Traemos el id de la consulta recien guardada, usando el codigo unico que generamos.
-		$traerConsulta = "SELECT idConsulta FROM consulta WHERE Observaciones = '".$codigo."';";
+		$traerConsulta = "SELECT idConsulta FROM consulta WHERE Observaciones like binary '".$codigo."';";
 		$runConsulta = mysqli_query($conexion, $traerConsulta);
 		$arrayConsulta = mysqli_fetch_array($runConsulta);
 		#Tenemos el id, lo mandamos a la variable; sera utilizado para insertar el examen fisico
 		$idConsulta = $arrayConsulta['0'];
 
-		print_r($idConsulta);
+		$examenFisico = "INSERT INTO examenfisico(PresionArterial, FrecuenciaCardiaca, Temperatura, SaturacionOxigeno, FrecuenciaRespiratoria, PielFameras, Cabeza, Cuello, Torax, Pulmones, Corazon, Abdomen, Ginecologico, Extremidades, Neurologico, Consulta_idConsulta, Consulta_Paciente_idPaciente) VALUES ('".$presion."','".$frecuencia."','".$temperatura."','".$saturacion."','".$respiratoria."','".$piel."','".$cabeza."','".$cuello."','".$torax."','".$pulmones."','".$corazon."','".$abdomen."','".$ginecologico."','".$extremidades."','".$neurologico."','".$idConsulta."','".$paciente."')";
+		$insertarExamenFisico = mysqli_query($conexion,$examenFisico);
+
+		if (!$insertarExamenFisico) {
+		
+	   printf("Errormessage: %s\n", $conexion->error);
 
 		}
+	}
 ?>
